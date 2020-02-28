bb82bd02f94abb5bd7ec458e7c9e156d0719346e
Angelo <7332321+antjowie@users.noreply.github.com>
2020-02-28 12:36:50 +0100

    Able to render multiple cubes

--- src/Main.cpp
+++ src/Main.cpp
@@ -8,6 +8,24 @@ public:
         return m_toWorld;
     }
 
+    void Update(float dt)
+    {
+        m_dt += dt;
+
+        auto rot = glm::mat4(1);
+        rot = glm::rotate(rot, glm::radians(m_dt * 50), { 1.f, 0.f, 0.f });
+        rot = glm::rotate(rot, glm::radians(m_dt * 30), { 0.f, 1.f, 0.f });
+        rot = glm::rotate(rot, glm::radians(m_dt * 90), { 0.f, 0.f, 1.f });
+    
+        m_toWorld = glm::scale(glm::mat4(1), glm::vec3(m_scale));
+        m_toWorld = rot * m_toWorld;
+        m_toWorld = glm::translate(glm::mat4(1), m_offset * m_scale) * m_toWorld;
+    }
+
+
+    float m_dt{ 0 };
+    float m_scale;
+    glm::vec3 m_offset;
     glm::mat4 m_toWorld{ 1 };
 };
 
@@ -16,18 +34,17 @@ class Game : public Engine
 private:
     virtual void OnInit()
     {
-        m_cubes.emplace_back();
-        //m_cubes.back().m_toWorld = 
-        return;
+        constexpr int size = 2;
 
-        for (int x = -16; x < 16; x++)
+        for (float x = -size; x < size; x++)
         {
-            for (int y = -16; y < 16; y++)
+            for (float y = -size; y < size; y++)
             {
-                for (int z = -16; z < 16; z++)
+                for (float z = -size; z < size; z++)
                 {
                     m_cubes.emplace_back();
-                    m_cubes.back().m_toWorld = glm::translate(glm::mat4(1), glm::fvec3(x, y, z + 15));
+                    m_cubes.back().m_offset = glm::fvec3(x*2 + 0.5f, y*2 + 0.5f, (z + size) * 2);
+                    m_cubes.back().m_scale = 1.f / (size * size * size);
                 }
             }
         }
@@ -35,24 +52,18 @@ private:
 
     virtual void OnUpdate(Time dt) override final
     {
-        static Time elapsed = 0;
-        elapsed += dt;
-
-        // Calculate MVP
-        const auto project = glm::perspective(glm::radians(45.f), (float)GetWindowSize().x / (float)GetWindowSize().y, 0.1f, 100.f);
-        const auto view = glm::translate(glm::mat4(1), glm::vec3(0.f, 0.f, -5.f));
-        auto& model = m_cubes.back().m_toWorld;
-        model = glm::mat4(1);
-        model = glm::rotate(model, glm::radians(elapsed * 50), { 1.f, 0.f, 0.f });
-        model = glm::rotate(model, glm::radians(elapsed * 30), { 0.f, 1.f, 0.f });
-        model = glm::rotate(model, glm::radians(elapsed * 90), { 0.f, 0.f, 1.f });
-
-        m_vp = project * view;
+        // Calculate models
+        for (auto& cube : m_cubes)
+        {
+            cube.Update(dt);
+        }
     }
 
     virtual void OnRender(Renderer &renderer) override final
     {
-        renderer.SetVP(m_vp);
+        const auto project = glm::perspective(glm::radians(45.f), (float)GetWindowSize().x / (float)GetWindowSize().y, 0.1f, 100.f);
+        const auto view = glm::translate(glm::mat4(1), glm::vec3(0.f, 0.f, -5.f));
+        renderer.SetVP(project * view);
 
         for (const auto& cube : m_cubes)
             cube.Render(renderer);
