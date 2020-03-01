70042b4f3e170ef5a53d9748e5d088db11ee0999
Angelo <7332321+antjowie@users.noreply.github.com>
2020-03-01 19:26:54 +0100

    Changed texture type to rgba so that it can load

--- src/graphics/Renderer.cpp
+++ src/graphics/Renderer.cpp
@@ -80,12 +80,13 @@ Renderer::Renderer(Config config)
     //glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_NEAREST);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-    // load and generate the texture
+    
+    // Load and generate the texture
     int width, height, nrChannels;
     unsigned char* data = stbi_load("res/texture.png", &width, &height, &nrChannels, 0);
     if (data)
     {
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
         glGenerateMipmap(GL_TEXTURE_2D);
     }
     else
